b6a91eef31e93d35b9134c154652de43218c8531
vlada967 <vladaarhipova3@gmail.com>
2022-12-06 20:38:30 +0700

    added las-reader

---
+++ myFirstProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using Ipgg.LasParser;
+
+public class LasReader
+{
+    public string saveDict(string namefile, string charset)
+    {
+        var parser = new LLasParserVlasov();
+        parser.ReadFile(namefile, charset);
+        return ((string.Join(Environment.NewLine, (parser.Data).Select(a => $"{a.Key}: {a.Value}"))));
+
+    }
+}
+
