d93bdd9c3905e74278cb07cc5d43ef22dc6d109f
Petr <p.samoseiko@g.nsu.ru>
2022-12-07 18:38:38 +0700

    refitted LAS reader

---
+++ myFirstProject/LearningCSharp/LasReader.cs
@@ -0,0 +1,14 @@
+﻿namespace LearningCSharp;
+
+using Ipgg.LasParser;
+
+public class LasReader
+{
+    private readonly LLasParserVlasov _parser = new LLasParserVlasov();
+
+    public string SaveDict(string namefile, string charset)
+    {
+        _parser.ReadFile(namefile, charset);
+        return ((string.Join(Environment.NewLine, (_parser.Data).Select(a => $"{a.Key}: {a.Value}"))));
+    }
+}
