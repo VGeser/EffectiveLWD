ca430ce6468ddbe13197f6f66f5b521c5a7a8c2d
Petr <p.samoseiko@g.nsu.ru>
2022-12-08 21:16:29 +0700

    cleared main class

--- myFirstProject/PMM-Simulator/MainClass.cs
+++ myFirstProject/PMM-Simulator/MainClass.cs
@@ -4,12 +4,6 @@ public class MainClass
 {
     public static void Main()
     {
-        LasReader thing = new LasReader();
-        Dictionary<String, Double?[]> map = thing.SaveDict(@"C:\Users\Synchro\Downloads\Считанные.mpp.las", "utf-8");
-        foreach (KeyValuePair<String, Double?[]> pair in map)
-        {
-            Console.Write(pair.Key + " ");
-            Console.WriteLine(pair.Value.Length);
-        }
+        
     }
 }
