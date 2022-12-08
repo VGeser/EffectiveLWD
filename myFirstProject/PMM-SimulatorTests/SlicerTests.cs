9cb5a13278a55f4ecf89b2cc0e634ede76889057
Petr <p.samoseiko@g.nsu.ru>
2022-12-08 14:23:50 +0700

    added tetst for slicer and test data

---
+++ myFirstProject/PMM-SimulatorTests/SlicerTests.cs
@@ -0,0 +1,26 @@
+using LearningCSharp;
+
+namespace MMM_SimulatorTests;
+
+public class SlicerTests
+{
+    private Slicer _slicer;
+    private Dictionary<String, Double?[]> _dictionary;
+
+    public SlicerTests()
+    {
+        LasReader reader = new LasReader();
+        _dictionary = reader.SaveDict(@"..\..\..\testdata\2022_09_21_21_18_13_277.gti.las", "utf-8");
+        this._slicer = new Slicer(_dictionary);
+    }
+
+    [Test]
+    public void TestSlicing()
+    {
+        Dictionary<String, Double?> dictionary = _slicer.getSlice(0);
+        foreach (KeyValuePair<String, Double?> pair in dictionary)
+        {
+            Assert.AreEqual(pair.Value, _dictionary[pair.Key][0]);
+        }
+    }
+}
